4f9d6fef34a70d9f94a004442653f11f95a61e76
sigvardbratlie <sigvard.bratlie@gmail.com>
2025-09-08 11:19:56 +0200

    added chat.

--- my_first_app.py
+++ my_first_app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import numpy as np
 
 st.title("Hello Streamlit-er 👋")
 st.markdown(
@@ -12,6 +13,13 @@ st.markdown(
     with Streamlit. 
     """
 )
+
+with st.chat_message("user"):
+    st.write("Hello 👋")
+
+with st.chat_message("assistant"):
+    st.write("Hello human")
+    st.bar_chart(np.random.randn(30, 3))
 
 if st.button("Send balloons!"):
     st.balloons()
