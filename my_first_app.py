b93848c78617a39635ee77568a0d248a4eafe3e1
sigvardbratlie <sigvard.bratlie@gmail.com>
2025-09-08 11:21:03 +0200

    added chat input

--- my_first_app.py
+++ my_first_app.py
@@ -17,9 +17,9 @@ st.markdown(
 with st.chat_message("user"):
     st.write("Hello 👋")
 
-with st.chat_message("assistant"):
-    st.write("Hello human")
-    st.bar_chart(np.random.randn(30, 3))
+prompt = st.chat_input("Say something")
+if prompt:
+    st.write(f"User has sent the following prompt: {prompt}")
 
 if st.button("Send balloons!"):
     st.balloons()
