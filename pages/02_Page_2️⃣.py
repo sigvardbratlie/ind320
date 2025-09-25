487131101c0927d31a52469723643e0c933d7360
sigvardbratlie <sigvard.bratlie@gmail.com>
2025-09-25 19:17:52 +0200

    added comments

--- pages/02_Page_2️⃣.py
+++ pages/02_Page_2️⃣.py
@@ -5,27 +5,26 @@ import os
 st.set_page_config(layout="wide")
 st.title("CA1 - Page 2")
 
-st.cache_data(show_spinner=False)
+st.cache_data(show_spinner=False) #using cache data to only read in the first time. Spinner false as it disappears fast
 def read_data(filepath):
     df = pd.read_csv(filepath)
-    df["time"] = pd.to_datetime(df["time"])
-    df = df.set_index("time")
+    df["time"] = pd.to_datetime(df["time"]) #setting date feature to dtype datetime
+    df = df.set_index("time") #Setting date feature as index
     return df
 
-df = read_data("data/open-meteo-subset.csv")
+df = read_data("data/open-meteo-subset.csv") 
 
-fy,fm = df.index.min().year, df.index.min().month
-first_month = df.loc[(df.index.year == fy) & df.index.month == fm, :]
+fy,fm = df.sort_index().index[0].year, df.sort_index().index[0].month #extracting the first month from the data
 
-first_month = (first_month - first_month.mean()) / first_month.std()
-
+first_month = df.loc[(df.index.year == fy) & df.index.month == fm, :] #filtering only on the first month
+first_month = (first_month - first_month.mean()) / first_month.std() #normalize for comparison
 
 data_for_editor = []
 for col in first_month.columns:
     data_for_editor.append({
         "column": col,
         "data": first_month[col].tolist(), 
-    })
+    }) #creating the data container for LineChartColumn
 
 st.data_editor(
     data_for_editor,
@@ -44,4 +43,4 @@ st.data_editor(
     },
     use_container_width=True,
     hide_index=True
-)
+) #Creating the widget LineChartColumn showing data for the first month
--- pages/03_Page_3️⃣.py
+++ pages/03_Page_3️⃣.py
@@ -5,7 +5,7 @@ st.set_page_config(layout="wide")
 st.title("CA1 - Page 3")
 
 
-st.cache_data(show_spinner=False)
+st.cache_data(show_spinner=False) #same as page 2
 def read_data(filepath):
     df = pd.read_csv(filepath)
     df["time"] = pd.to_datetime(df["time"])
@@ -13,32 +13,31 @@ def read_data(filepath):
     return df
 
 df = read_data("data/open-meteo-subset.csv")
-df = (df-df.mean())/df.std()
+df = (df-df.mean())/df.std() #Normalize the data
 
-start_end = "E"
-date_agg_map = {"Year":f"Y{start_end}",f"Month":f"M{start_end}",f"Week":f"W",f"Day":f"D"}
+start_end = "E" #choosing either end or start with "E" or "S". Defualt "E", no option implemented
+date_agg_map = {"Year":f"Y{start_end}",f"Month":f"M{start_end}",f"Week":f"W",f"Day":f"D"} #a map for choosing the correct label from streamlit radio widget
 
-plot_type = st.selectbox("Choose plot type",options=["line","bar","hist"])
+plot_type = st.selectbox("Choose plot type",options=["line","bar","hist"]) #selection for plot type as no specific type is specified in the task description.
 
 if plot_type == "line":
-    months_select = st.select_slider("Select a subset of months to display",options = range(1,13), value=(1,12))
-    months = list(range(months_select[0],months_select[1]+1))
-    y = st.multiselect("Select columns to plot",options = df.columns)
-    y = y if y else df.columns
-    date_agg = st.radio("Choose date aggregation",  options=["Month","Week","Day"],index = 1,horizontal=True)
-    #st.info(f'month : {months, months_select}, y : {y}, date_agg : {date_agg}')
-    st.line_chart(df.loc[df.index.month.isin(months),y].resample(date_agg_map[date_agg]).mean()
-                )
+    months_select = st.select_slider("Select a subset of months to display",options = range(1,13), value=(1,12)) #creating the slider widget for selecting month range
+    months = list(range(months_select[0],months_select[1]+1)) #adding one as python index is zero-based
+    y = st.multiselect("Select columns to plot",options = df.columns) #Selection of y
+    y = y if y else df.columns #ensuring that y is not None
+    date_agg = st.radio("Choose date aggregation",  options=["Month","Week","Day"],index = 1,horizontal=True) #adding data aggregation option
+    st.line_chart(df.loc[df.index.month.isin(months),y].resample(date_agg_map[date_agg]).mean()) #creating the line chart
+                
     
 elif plot_type == "bar":
-    x = st.selectbox("Select which column to use as x-axis", options=df.columns)
+    x = st.selectbox("Select which column to use as x-axis", options=df.columns) #selecting values for x
     x = x #if x else df.columns[0]
-    y = st.multiselect("Select columns to plot",options = df.columns)
-    y = y if y else df.columns.tolist()
-    st.bar_chart(data = df, x = x, y = y)
+    y = st.multiselect("Select columns to plot",options = df.columns) #selecting values for y
+    y = y if y else df.columns.tolist() #ensuring that y is not None
+    st.bar_chart(data = df, x = x, y = y) #creating the bar chart
 
 elif plot_type == "hist":
-    x = st.selectbox("Select which column to use as x-axis", options=df.columns)
-    x = x if x else df.columns[0]
-    data = df.value_counts(x)
-    st.bar_chart(data)
+    x = st.selectbox("Select which column to use as x-axis", options=df.columns) #selecting values for x
+    x = x if x else df.columns[0] #ensuring x is not None
+    data = df.value_counts(x) #mk data
+    st.bar_chart(data) #plot data
