55d9226250f17ef39007a83afcde0ba7365f7577
sigvardbratlie <sigvard.bratlie@gmail.com>
2025-10-07 17:48:02 +0200

    changed titles.

--- 0_home.py
+++ 0_home.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.set_page_config(layout="wide") #setting page conig with layout wide to fill the page
 
-st.title("CA1 - Home page")
+st.title("Home page")
 st.header("Header 1")
 
 with st.sidebar:
