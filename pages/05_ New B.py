281f91fa33d26578c4d260b205356a593e46aead
sigvardbratlie <sigvard.bratlie@gmail.com>
2025-11-06 08:48:00 +0100

    updated choose col options in New B

--- pages/05_ New B.py
+++ pages/05_ New B.py
@@ -82,15 +82,6 @@ df = pd.DataFrame(data.get("hourly"))
 df["time"] = pd.to_datetime(df["time"])
 df.set_index("time", inplace=True)
 
- 
-
-col = st.radio(
-    "Select feature",
-    options = df.columns.tolist(),
-    index = df.columns.tolist().index("temperature_2m"),
-    horizontal=True,
-    label_visibility="collapsed",
-    )
 
 
 #===========================================
@@ -99,6 +90,13 @@ col = st.radio(
 tabs = st.tabs(["Outlier Detection", "LOF Analysis"])  #create tabs
 
 with tabs[0]:
+    col = st.radio(
+            "Select feature",
+            options = df.columns.tolist(),
+            index = df.columns.tolist().index("temperature_2m"),
+            horizontal=True,
+            label_visibility="collapsed",
+            )
     st.subheader("Outlier Detection")
     spc_selection = st.columns(2) #selections for outlier detection
     with spc_selection[0]:
@@ -110,6 +108,14 @@ with tabs[0]:
     st.plotly_chart(fig)
 
 with tabs[1]:
+    col = st.radio(
+            "Select feature",
+            options = df.columns.tolist(),
+            index = df.columns.tolist().index("precipitation"),
+            horizontal=True,
+            label_visibility="collapsed",
+            )
+    
     st.subheader("LOF Analysis")
     
     lof_select = st.columns(2) #selections for LOF
@@ -117,6 +123,9 @@ with tabs[1]:
         n_neighbors = st.slider("Number of neighbors", min_value=1, max_value=100, value=20, step=1)
     with lof_select[1]:
         contamination = st.slider("Contamination", min_value=0.01, max_value=0.1, value=0.01, step=0.01)
+
+    
+    
     
     fig = lof(df = df , feature = col, n_neighbors=n_neighbors, contamination=contamination)
     st.plotly_chart(fig)
